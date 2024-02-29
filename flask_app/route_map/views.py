70f3b88c0d1a9353236c81d8f4a7998fb026ce6d
情野陸 <rikuseino@jouyarikunoMacBook-Pro.local>
2024-02-29 21:36:40 +0900

    edit: edit resultbar design

--- flask_app/route_map/views.py
+++ flask_app/route_map/views.py
@@ -14,14 +14,10 @@ def execute_search(y_start, x_start, y_end, x_end, target_length):
         roundtrip_option = "true"
     else:
         roundtrip_option = "false"
-    url = f"https://trailrouter.com/ors/experimentalroutes?coordinates={x_start},{y_start}%7C{x_end},{y_end}&skip_segments=&green_preference=0.8&avoid_unsafe_streets=false&avoid_unlit_streets=false&hills_preference=0&avoid_repetition=true&target_distance={target_length}&roundtrip={roundtrip_option}"
+    url = f"https://trailrouter.com/ors/experimentalroutes?coordinates={x_start},{y_start}%7C{x_end},{y_end}&skip_segments=&green_preference=&avoid_unsafe_streets=false&avoid_unlit_streets=false&hills_preference=0&avoid_repetition=true&target_distance={target_length}&roundtrip={roundtrip_option}"
     res = requests.get(url)
     data = res.json()
     return data
-    route = data["routes"][0]["geometry"]["coordinates"]
-    route_length = data["routes"][0]["distance"]
-    way_point_indices = data["routes"][0]["waypointIndices"]
-    return route, route_length, way_point_indices
 
 
 @route_map.route("/",methods=["POST","GET"])
