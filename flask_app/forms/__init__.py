1fffd112d11ab6bb69466919da716e27b7f4016f
情野陸 <rikuseino33@gmail.com>
2024-06-30 12:26:22 +0900

    Add course forms

--- flask_app/forms/__init__.py
+++ flask_app/forms/__init__.py
@@ -1,4 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, FileField, ValidationError
+from wtforms import PasswordField, StringField, SubmitField, FileField, TextAreaField, SelectField,widgets,ValidationError
 from wtforms.validators import DataRequired, Email, Length
+from wtforms_sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
+from flask_app.models.address import Prefecture
+from flask_app.models.facilities import Facility
 import re
---
+++ flask_app/forms/course_forms.py
@@ -0,0 +1,42 @@
+from flask_app.forms import *
+
+def prefecture_query():
+    return Prefecture.query
+
+def facility_query():
+    return Facility.query
+
+class CreateCourseForm(FlaskForm):
+    title = StringField(
+        "コースのタイトル",
+        validators=[
+            DataRequired("コースタイトルは必須です。"),
+            Length(1, 50, "50文字以内で入力してください。")
+        ],
+    )
+    description = TextAreaField(
+        "コースの説明",
+        validators=[
+            DataRequired("コースの説明は必須です。"),
+            Length(1, 200, "200文字以内で入力してください。")
+        ]
+    )
+    prefecture = QuerySelectField(
+        "都道府県",
+        query_factory=prefecture_query,
+        get_label='name',
+        allow_blank=False,
+        validators=[DataRequired("都道府県は必須です。")]
+    )
+    city = SelectField(
+        "市区町村",
+        choices=[],
+        validate_choice=False
+    )
+    facilities = QuerySelectMultipleField(
+        "近くの施設",
+        query_factory=facility_query,
+        get_label='name',
+        widget=widgets.ListWidget(prefix_label=False),
+        option_widget=widgets.CheckboxInput()
+    )
